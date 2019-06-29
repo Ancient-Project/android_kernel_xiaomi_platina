f6c42ee827a902c8dfc9ffe2582052b99a5337a6
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-06-29 18:13:37 +0700

    insert build.sh

---
+++ build_dirty.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#
+# Copyright © 2018, "Vipul Jha" aka "LordArcadius" <vipuljha08@gmail.com>
+# Copyright © 2018, "penglezos" <panagiotisegl@gmail.com>
+# Copyright © 2018, "reza adi pangestu" <rezaadipangestu385@gmail.com>
+# Copyright © 2018, "beamguide" <beamguide@gmail.com>
+
+BUILD_START=$(date +"%s")
+blue='\033[0;34m'
+cyan='\033[0;36m'
+green='\e[0;32m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+purple='\e[0;35m'
+white='\e[0;37m'
+
+KERNEL_DIR=$PWD
+REPACK_DIR=$KERNEL_DIR/zip
+OUT=$KERNEL_DIR/out
+ZIP_NAME="$VERSION"-"$DATE"
+VERSION="platina"
+DATE=$(date +%Y%m%d-%H%M)
+
+export KBUILD_BUILD_USER=builder
+export KBUILD_BUILD_HOST=ancientdedicated
+export ARCH=arm64
+export SUBARCH=arm64
+export USE_CCACHE=1
+export CLANG_PATH=/root/reza/clang2/bin
+export PATH=${CLANG_PATH}:${PATH}
+export CLANG_TRIPLE=aarch64-linux-gnu-
+export CROSS_COMPILE=/root/reza/gcc/bin/aarch64-linux-android-
+export CROSS_COMPILE_ARM32=/root/reza/gcc2/bin/arm-linux-androideabi-
+export CLANG_TCHAIN="/root/reza/clang2/bin/clang"
+export KBUILD_COMPILER_STRING="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
+
+make_zip()
+{
+                cd $REPACK_DIR
+                mkdir kernel
+                mkdir dtbs
+                #cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz $REPACK_DIR/kernel/
+                rm $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/modules.order
+                cp $KERNEL_DIR/out/arch/arm64/boot/dts/qcom/* $REPACK_DIR/dtbs/
+                cp $KERNEL_DIR/out/arch/arm64/boot/Image.gz $REPACK_DIR/kernel/
+		FINAL_ZIP="Perf-${VERSION}-${DATE}.zip"
+        zip -r9 "${FINAL_ZIP}" *
+		cp *.zip $OUT
+		rm *.zip
+                rm -rf kernel
+                rm -rf dtbs
+		cd $KERNEL_DIR
+		rm out/arch/arm64/boot/Image.gz
+}
+
+make platina_defconfig O=out/
+make -j$(nproc --all) O=out/
+make_zip
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+rm -rf zip/kernel
+rm -rf zip/dtbs
+echo -e ""
+echo -e ""
+echo -e "ANCIENT KERNEL"
+echo -e ""
+echo -e ""
+echo -e "Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
---
+++ zip/anykernel.sh
@@ -0,0 +1,85 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Testing
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=platina
+device.name2=platina-user
+device.name3=
+device.name4=
+device.name5=
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=0;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. /tmp/anykernel/tools/ak2-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# begin ramdisk changes
+
+# Remove CAF Boost Framework cuz CAF is a hoe
+mount -o rw,remount -t auto /vendor >/dev/null; 
+rm -rf /vendor/etc/perf;
+mount -o ro,remount -t auto /vendor >/dev/null;
+
+# init.rc
+insert_line init.rc 'ancient' after 'import /init.\${ro.zygote}.rc' 'import /init.ancient.rc';
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=/tmp/anykernel/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+# Concatenate all of the dtbs to the kernel
+  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
+fi;
+
+# Patch F2FS thanks Aradium
+vft=/vendor/etc/fstab.qcom;
+$bb mount -o rw,remount -t auto /system >/dev/null;
+$bb mount -o rw,remount -t auto /vendor 2>/dev/null;
+if [ -n $(grep "f2fs" $vft 2>/dev/null) != "" ]; then
+  ui_print "";
+  ui_print "";
+  ui_print "Patching F2FS...";
+backup_file $vft;
+insert_line $vft 'f2fs-data' after '/dev/block/bootdevice/by-name/userdata' '/dev/block/bootdevice/by-name/userdata   /data                  f2fs   noatime,nosuid,nodev,nodiratime,fsync_mode=nobarrier,background_gc=off   wait,check,encryptable=footer,crashcheck,quota,reservedsize=128M';
+insert_line $vft 'f2fs-cache' after '/dev/block/bootdevice/by-name/cache' '/dev/block/bootdevice/by-name/cache      /cache                 f2fs   noatime,nosuid,nodev,nodiratime,discard,fsync_mode=nobarrier,inline_xattr,inline_data,data_flush   wait';
+else
+  ui_print "";
+  ui_print "";
+  ui_print "Your ROM already support F2FS partitions";
+fi;
+$bb mount -o ro,remount -t auto /system >/dev/null;
+$bb mount -o ro,remount -t auto /vendor 2>/dev/null;
+
+# end ramdisk changes
+
+write_boot;
+
+## end install
