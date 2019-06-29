ca6c356621652ef809a2fea4e2292b897849ec84
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-06-30 01:19:31 +0700

    defconfig: beta2

--- build_dirty.sh
+++ build_dirty.sh
@@ -19,7 +19,7 @@ KERNEL_DIR=$PWD
 REPACK_DIR=$KERNEL_DIR/zip
 OUT=$KERNEL_DIR/out
 ZIP_NAME="$VERSION"-"$DATE"
-VERSION="platina-beta"
+VERSION="platina-beta2"
 DATE=$(date +%Y%m%d-%H%M)
 
 export KBUILD_BUILD_USER=builder
