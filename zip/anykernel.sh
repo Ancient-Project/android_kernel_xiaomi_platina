53847be16e1edf7aa56281bbd98c7419255d4e93
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-07-17 20:49:37 +0700

    zip: update

--- zip/anykernel.sh
+++ zip/anykernel.sh
@@ -1,10 +1,10 @@
-# AnyKernel2 Ramdisk Mod Script
+# AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Ancient Kernel From Indonesia
+kernel.string=Ancient Kernel
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -14,6 +14,7 @@ device.name2=platina-user
 device.name3=MI 8 Lite
 device.name4=Xiaomi
 device.name5=
+supported.versions=
 '; } # end properties
 
 # shell variables
@@ -21,43 +22,25 @@ block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
+
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
-. /tmp/anykernel/tools/ak2-core.sh;
+. tools/ak3-core.sh;
+
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
 chmod -R 750 $ramdisk/*;
 chown -R root:root $ramdisk/*;
 
+
 ## AnyKernel install
 dump_boot;
 
 # begin ramdisk changes
 
-# Remove CAF Boost Framework cuz CAF is a hoe
-mount -o rw,remount -t auto /vendor >/dev/null; 
-rm -rf /vendor/etc/perf;
-mount -o ro,remount -t auto /vendor >/dev/null;
-
 # init.rc
-insert_line init.rc 'ancient' after 'import /init.\${ro.zygote}.rc' 'import /init.ancient.rc';
-
-# If the kernel image and dtbs are separated in the zip
-decompressed_image=/tmp/anykernel/kernel/Image
-compressed_image=$decompressed_image.gz
-if [ -f $compressed_image ]; then
-  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
-  if [ -d $ramdisk/.backup ]; then
-    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
-    $bin/magiskboot --decompress $compressed_image $decompressed_image;
-    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
-    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
-  fi;
-
-# Concatenate all of the dtbs to the kernel
-  cat $compressed_image /tmp/anykernel/dtbs/*.dtb > /tmp/anykernel/Image.gz-dtb;
-fi;
+#insert_line init.rc 'ancient' after 'import /init.\${ro.zygote}.rc' 'import /init.ancient.rc';
 
 # Patch F2FS thanks Aradium
 vft=/vendor/etc/fstab.qcom;
@@ -81,5 +64,5 @@ $bb mount -o ro,remount -t auto /vendor 2>/dev/null;
 # end ramdisk changes
 
 write_boot;
-
 ## end install
+
