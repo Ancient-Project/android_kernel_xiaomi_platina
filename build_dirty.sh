b5bb30266f8dd66dc3cd75b365d8cfb673aef6e7
rezaadi0105 <rezaadipangestu5@gmail.com>
2019-08-05 23:03:45 +0700

    ancient 1.3

--- build_dirty.sh
+++ build_dirty.sh
@@ -19,7 +19,7 @@ KERNEL_DIR=$PWD
 REPACK_DIR=$KERNEL_DIR/zip
 OUT=$KERNEL_DIR/out
 ZIP_NAME="$VERSION"-"$DATE"
-VERSION="platina-GlobStable-1.2"
+VERSION="platina-GlobStable-1.3"
 DATE=$(date +%Y%m%d-%H%M)
 
 export KBUILD_BUILD_USER=builder
